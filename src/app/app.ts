0273f1d59073c529ea73eec2b36fef3973254f4a
Komal Rajgadkar <rajgadkarkomal1994@gmail.com>
2025-09-18 17:05:29 +0530

    added components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './Components/header/header';
+import { Home } from './Components/home/home';
+import { Footer } from './Components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Header,Home,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
